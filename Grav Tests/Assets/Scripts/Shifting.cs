7865acaf0fd93239706c70c81e1c1329187ff70c
Postboredom <noah1010@me.com>
2019-06-25 20:26:18 -0700

    couple of touches

--- Grav Tests/Assets/Scripts/Shifting.cs
+++ Grav Tests/Assets/Scripts/Shifting.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class Shifting : MonoBehaviour {
     [SerializeField] private Vector3 gravity;
-    [SerializeField] private float smooth = 1.0f;
+    [SerializeField] private float smooth = 10.0f;
     [SerializeField] private float time = 0.5f;
     [SerializeField] private Vector3 targetAngles;
-
+    private void Start()
+    {
+        targetAngles = transform.position;
+    }
     void Update () {
 
         gravity = Physics.gravity;
         if (Input.GetButtonDown("Shift"))
         {
             Physics.gravity = -gravity;
-            targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
+            targetAngles = transform.eulerAngles + 180f * Vector3.forward; // what the new angles should be
 
         }
-
-        if (targetAngles != transform.eulerAngles)
-        {
             transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, smooth * Time.deltaTime); // lerp to new angles
-        }
+        
     }
 }
