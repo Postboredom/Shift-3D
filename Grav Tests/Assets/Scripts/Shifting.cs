d1cba01d04aa34722c3410ab0caf9c1a0899fc09
Postboredom <noah1010@me.com>
2019-06-25 18:57:52 -0700

    getting main gameplay loop working

---
+++ Grav Tests/Assets/Scripts/Shifting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shifting : MonoBehaviour {
+    [SerializeField] private Vector3 gravity;
+    [SerializeField] private float smooth = 1.0f;
+    [SerializeField] private float time = 0.5f;
+    [SerializeField] private Vector3 targetAngles;
+
+    void Update () {
+
+        gravity = Physics.gravity;
+        if (Input.GetButtonDown("Shift"))
+        {
+            Physics.gravity = -gravity;
+            targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
+
+        }
+
+        if (targetAngles != transform.eulerAngles)
+        {
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, smooth * Time.deltaTime); // lerp to new angles
+        }
+    }
+}
